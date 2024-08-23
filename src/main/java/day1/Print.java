34707121110e6bb1bd191bbbb17e80e138141802
chacha86 <taejincha0619@gmail.com>
2024-08-23 09:34:38 +0900

    comment

---
+++ src/main/java/day1/Print.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Print {
+
+    // 주석(comment) : 사람이 보기 위한 코멘트. 컴퓨터는 무시합니다. (한줄 주석)
+    public static void main(String args[]) {
+        // 여러줄 주석 : 컨트롤 + /
+//        aaa;
+//        bbb;
+//        ccc;
+//        ddd;
+
+
+    }
+}
