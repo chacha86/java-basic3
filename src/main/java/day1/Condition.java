a3272e0b0dfbda89be363c8d2da428fbd12390bc
chacha86 <taejincha0619@gmail.com>
2024-08-23 13:35:33 +0900

    weather problem solve

--- src/main/java/day1/Condition.java
+++ src/main/java/day1/Condition.java
@@ -4,18 +4,18 @@ public class Condition {
     public static void main(String[] args) {
         // 조건
         // if 소중
-        if(false) {
+        if (false) {
             System.out.println("안녕");
         }
 
-        if(false) {
+        if (false) {
             System.out.println("바이");
         }
 
         int num;
         num = 15;
 
-        if(num > 10) {
+        if (num > 10) {
             System.out.println("num이 10보다 크다");
         }
 
@@ -24,10 +24,10 @@ public class Condition {
 
         // 1만원 이상 : 치킨
         // 1만원 미만 : 떡볶이
-        if(money >= 10000) {
+        if (money >= 10000) {
             System.out.println("치킨을 먹어요");
         }
-        if(money < 10000) {
+        if (money < 10000) {
             System.out.println("떡볶이를 먹어요");
         }
 
@@ -39,6 +39,29 @@ public class Condition {
         int weather;
         weather = 1; // 1. 맑음, 2. 흐림, 3. 비, 4. 눈
 
+        // 실행될 경우의 수가 4 -> if 문 4개
+        if(weather == 1) {
+            System.out.println("날씨가 맑습니다. 나들이 나가세요.");
+        }
+        if(weather == 2) {
+            System.out.println("날씨가 흐립니다. 집에 계세요.");
+        }
+        if(weather == 3) {
+            System.out.println("날씨가 비가 옵니다. 우산을 챙기세요.");
+        }
+        if(weather == 4) {
+            System.out.println("날씨가 눈이 옵니다. 눈사람 만들어보세요.");
+        }
+
+        // 나이에 따른 학적 표현
+        // 초등학생, 중학생, 고등학생, 성인
+        // 13살 이하는 초등학생.
+        // 16살 이하는 중학생
+        // 19살 이하는 고등학생
+        // 20살 이상은 성인
+
+        int age;
+        age = 17;
 
 
 
