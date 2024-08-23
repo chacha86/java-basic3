c8fca3b1b4823f82b67bd191306aa709f485770b
chacha86 <taejincha0619@gmail.com>
2024-08-23 13:13:38 +0900

    condition

---
+++ src/main/java/day1/Condition.java
@@ -0,0 +1,46 @@
+package day1;
+
+public class Condition {
+    public static void main(String[] args) {
+        // 조건
+        // if 소중
+        if(false) {
+            System.out.println("안녕");
+        }
+
+        if(false) {
+            System.out.println("바이");
+        }
+
+        int num;
+        num = 15;
+
+        if(num > 10) {
+            System.out.println("num이 10보다 크다");
+        }
+
+        int money;
+        money = 1000;
+
+        // 1만원 이상 : 치킨
+        // 1만원 미만 : 떡볶이
+        if(money >= 10000) {
+            System.out.println("치킨을 먹어요");
+        }
+        if(money < 10000) {
+            System.out.println("떡볶이를 먹어요");
+        }
+
+        // 맑음 : 날씨가 맑습니다. 나들이 나가세요.
+        // 흐림 : 날씨가 흐립니다. 집에 계세요.
+        // 비 : 날씨가 비가 옵니다. 우산을 챙기세요.
+        // 눈 : 날씨가 눈이 옵니다. 눈사람 만들어보세요.
+
+        int weather;
+        weather = 1; // 1. 맑음, 2. 흐림, 3. 비, 4. 눈
+
+
+
+
+    }
+}
