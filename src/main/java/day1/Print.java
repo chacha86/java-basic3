30d327af554ee949bdfc22727627c9b1e52d6b77
chacha86 <taejincha0619@gmail.com>
2024-08-23 10:27:25 +0900

    problem solve

--- src/main/java/day1/Print.java
+++ src/main/java/day1/Print.java
@@ -36,6 +36,7 @@ public class Print {
         // 줄바꿈 문자 : \n
         System.out.println("안녕하세요\n반갑습니다.");
 
+        // 실습 문제
         // 구구단 2단을 System.out.println 한번만 사용해서 출력해주세요.
         // 2 X 1 = 2
         // 2 X 2 = 4
@@ -43,6 +44,8 @@ public class Print {
         // 2 X 4 = 8
         // ....
         // 2 X 9 = 18
+
+        System.out.println("2 X 1 = " + (2*1) + "\n2 X 2 = " + (2*2) + "\n2 X 3 = " + (2*3) + "\n2 X 4 = " + (2*4) + "\n2 X 5 = " + (2*5));
 
     }
 }
