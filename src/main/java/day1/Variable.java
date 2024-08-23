4a03493589322b90c4e2d9477f48100c394abb1c
chacha86 <taejincha0619@gmail.com>
2024-08-23 10:42:42 +0900

    variable

---
+++ src/main/java/day1/Variable.java
@@ -0,0 +1,44 @@
+package day1;
+
+public class Variable {
+    public static void main(String[] args) {
+        // 홍길동의 나이가 20.
+        // 홍길동 나이가 21로 올라감.
+        System.out.println(21);
+        System.out.println(21);
+        System.out.println(21);
+        System.out.println(21);
+        System.out.println(21);
+
+        // 변수를 사용해서 값을 저장할 수 있다.
+
+        // 변수는 상자
+        // 상자는 이름이 있어야 함.
+        // 상자는 크기와 모양이 있어야 함.
+
+        // 숫자
+        // 상자(변수)를 만들어야 함.
+        int age;
+
+        // 값을 변수에 담아야함.
+        // = 을 이용해서 저장 가능
+        age = 20;
+
+        // 문자
+        String name;
+        name = "홍길동";
+
+        // 변수의 값을 꺼내서 사용
+        System.out.println(age);
+        System.out.println(name);
+
+        // 안녕하세요 저는 20살 홍길동입니다. 10번 출력.
+
+        // 홍길동의 나이가 21살이 되었습니다. 21살로 바꿔서 10번 출력해주세요.
+
+        // 홍길동이 홍길만으로 개명을 했습니다. 바뀐 이름을 적용해서 10번 출력해주세요.
+
+
+
+    }
+}
