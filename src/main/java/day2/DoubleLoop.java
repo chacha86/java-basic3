5bba2ff20409de64a60750919c164f956f6360b7
chacha86 <taejincha0619@gmail.com>
2024-08-26 09:47:06 +0900

    day2 - double loop

---
+++ src/main/java/day2/DoubleLoop.java
@@ -0,0 +1,46 @@
+package day2;
+
+public class DoubleLoop {
+    public static void main(String[] args) {
+        // while, for
+
+
+
+
+        // 구구단 2단 만들기
+
+        // 2 X 1 = 2
+        // 2 X 2 = 4
+        // 2 X 3 = 6
+        // ...
+        // 2 X 9 = 18
+
+        // 개인이 최대한 고민할 수 있는 시간 정한다.(30분)
+        // 검색(구글, gpt)을 통해 해결()
+        // 저한테 질문
+
+
+        // 2단을 -> 3단으로 바꿔주세요.
+
+        // 1 , 2 , 3, 4
+//        for(int i = 1; i <= 4; i++) {
+//            System.out.println(dan + " X " + i + " = " + (dan * i));
+//        }
+
+
+        // hi 100번 출력
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("hi");
+            }
+        }
+
+        for(int dan2 = 2; dan2 <= 9; dan2++) {
+            for(int i = 1; i <= 4; i++) {
+                System.out.println(dan2 + " X " + i + " = " + (dan2 * i));
+            }
+        }
+
+    }
+}
