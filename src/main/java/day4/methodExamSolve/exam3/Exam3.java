067f2bc747fc5dc7b9bc9d51ee8489c2282f40eb
chacha86 <taejincha0619@gmail.com>
2024-08-28 17:23:58 +0900

    method exam3

---
+++ src/main/java/day4/methodExamSolve/exam3/Exam3.java
@@ -0,0 +1,11 @@
+package day4.methodExamSolve.exam3;
+
+public class Exam3 {
+    public static void main(String[] args) {
+        Test t1 = new Test();
+
+        t1.print(4); // 출력: 4
+        t1.print(10); // 출력: 10
+        t1.print(100); // 출력: 100
+    }
+}
---
+++ src/main/java/day4/methodExamSolve/exam3/Test.java
@@ -0,0 +1,7 @@
+package day4.methodExamSolve.exam3;
+
+public class Test {
+    public void print(int num) {
+        System.out.println(num);
+    }
+}
