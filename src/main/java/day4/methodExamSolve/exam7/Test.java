7d07e026981d0aad93d414fe03ddce0d20aaa172
chacha86 <taejincha0619@gmail.com>
2024-08-28 17:30:54 +0900

    method exam7

---
+++ src/main/java/day4/methodExamSolve/exam7/Test.java
@@ -0,0 +1,19 @@
+package day4.methodExamSolve.exam7;
+
+public class Test {
+    public int func1() {
+        return 10;
+    }
+
+    public int func2(int num) {
+        return num;
+    }
+
+    public int func3(int num) {
+        return num * num;
+    }
+
+    public int func4(int num1, int num2) {
+        return num1 + num2;
+    }
+}
