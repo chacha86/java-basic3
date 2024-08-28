30f878fe35d4daf03652b6f01e4c7f06dc59179d
chacha86 <taejincha0619@gmail.com>
2024-08-28 17:25:26 +0900

    method exam4

---
+++ src/main/java/day4/methodExamSolve/exam4/Person.java
@@ -0,0 +1,9 @@
+package day4.methodExamSolve.exam4;
+
+public class Person {
+    public void introduce() {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("안녕하세요. 저는 20살 홍길동입니다.");
+        }
+    }
+}
