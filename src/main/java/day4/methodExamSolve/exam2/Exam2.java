66ae551b6dad8c500391fd7259dfa19a0b42402c
chacha86 <taejincha0619@gmail.com>
2024-08-28 17:22:47 +0900

    method exam2

---
+++ src/main/java/day4/methodExamSolve/exam2/Exam2.java
@@ -0,0 +1,15 @@
+package day4.methodExamSolve.exam2;
+
+public class Exam2 {
+    public static void main(String[] args) {
+
+        Test t1 = new Test();
+
+        t1.hi();
+        // 안녕하세요. 저는 홍길동입니다. 잘부탁드립니다.
+
+        t1.bye();
+        // 안녕히 가세요. 다음에 또 봐요!
+
+    }
+}
---
+++ src/main/java/day4/methodExamSolve/exam2/Test.java
@@ -0,0 +1,11 @@
+package day4.methodExamSolve.exam2;
+
+public class Test {
+    public void hi() {
+        System.out.println("안녕하세요. 저는 홍길동입니다. 잘부탁드립니다.");
+    }
+
+    public void bye() {
+        System.out.println("안녕히 가세요. 다음에 또 봐요!");
+    }
+}
