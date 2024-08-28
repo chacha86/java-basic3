ec5f9e239db66299b84e40b99857ed5e3e8c8bca
chacha86 <taejincha0619@gmail.com>
2024-08-28 15:50:43 +0900

    2번 기능 완료

--- src/main/java/day4/vending/VendingMachineApp.java
+++ src/main/java/day4/vending/VendingMachineApp.java
@@ -74,20 +74,30 @@ public class VendingMachineApp {
                 remainder = remainder + money; // 잔액에 투입 금액 합산
 
             } else if(menu == 2) {
+                System.out.print("음료수를 선택해주세요 : ");
+                int target = Integer.parseInt(sc.nextLine());
+
+                if(prices[target] > remainder) {
+                    System.out.println("잔액이 부족합니다.");
+                    continue;
+                }
+
+                System.out.println(names[target] + "를 뽑았습니다.");
+                remainder = remainder - prices[target]; // 잔액에서 음료수 가격 차감
+                System.out.println("잔액은 " + remainder + " 원입니다.");
 
             } else if(menu == 3) {
                 System.out.println("== 음료수 목록 ==");
                 for(int i = 0; i < names.length; i++) {
                     System.out.println(i + ". " + names[i] + " : " + prices[i] + "원");
                 }
-
             } else if(menu == 4) {
                 System.out.println("현재 잔액은 " + remainder + "원 입니다.");
+
             } else if(menu == 5) {
                 System.out.println("자판기 프로그램을 종료합니다. 남은 돈 0원이 반환됩니다.");
                 break;
             }
-
         }
     }
 }
