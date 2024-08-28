ffa85c38d995ed0c56bce6454dc037458bbdc358
chacha86 <taejincha0619@gmail.com>
2024-08-28 17:21:19 +0900

    exam1 solve

---
+++ src/main/java/day4/methodExamSolve/exam1/Exam1.java
@@ -0,0 +1,17 @@
+package day4.methodExamSolve.exam1;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        // 메서드를 올바르게 호출해서 아래처럼 출력되게 해주세요.
+
+        Test t1 = new Test();
+        // 안녕하세요!!
+        t1.hi2();
+        // 저는 차태진입니다.
+        t1.hi1();
+        // 자바를 통해
+        t1.hi4();
+        // 프로그램을 만들어보아요.
+        t1.hi3();
+    }
+}
---
+++ src/main/java/day4/methodExamSolve/exam1/Test.java
@@ -0,0 +1,19 @@
+package day4.methodExamSolve.exam1;
+
+public class Test {
+    public void hi1() {
+        System.out.println("저는 차태진입니다.");
+    }
+
+    public void hi2() {
+        System.out.println("안녕하세요.");
+    }
+
+    public void hi3() {
+        System.out.println("프로그램을 만들어보아요.");
+    }
+
+    public void hi4() {
+        System.out.println("자바를 통해");
+    }
+}
