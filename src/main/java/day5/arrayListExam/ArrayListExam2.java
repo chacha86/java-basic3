20dfda98ff4582567634c8508cae4c73d97f6eb8
chacha86 <taejincha0619@gmail.com>
2024-08-29 13:52:13 +0900

    various arrayList of type

---
+++ src/main/java/day5/arrayListExam/ArrayListExam2.java
@@ -0,0 +1,26 @@
+package day5.arrayListExam;
+
+import java.util.ArrayList;
+
+public class ArrayListExam2 {
+    public static void main(String[] args) {
+
+
+
+        // int -> Integer
+
+        //ArrayList는 타입(자료형)을 객체만 다룬다.
+
+        // 객체형 자료형(리모콘), 원시형 자료형(int, boolean, double) 두가지로 구분.
+
+        // 원시형의 객체형 자료형을 제공
+        // int -> Integer
+        // char -> Character
+        // 원시형 타입명에서 앞글자만 대문자로 바꾸면 객체형
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<String> arr2 = new ArrayList<>();
+        ArrayList<Person> arr3 = new ArrayList<>();
+        ArrayList<Car> arr4 = new ArrayList<>();
+
+    }
+}
