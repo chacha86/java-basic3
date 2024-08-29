573ecc35ffed941a45108350980760f6ba1db62b
chacha86 <taejincha0619@gmail.com>
2024-08-29 16:44:12 +0900

    음료 선택 기능 완료

--- src/main/java/day5/kes/Main.java
+++ src/main/java/day5/kes/Main.java
@@ -62,6 +62,7 @@ public class Main {
 
         String[] drinkNames = {"콜라", "사이다", "커피"};
         int[] drinkPrices = {1000, 1200, 800};
+        int remainder = 0;
 
         while(true) {
             System.out.print("기능 선택 (1. 돈 투입,  2. 음료 선택,  3.음료 목록 확인, 4.잔액 확인, 5. 사용 종료) : ");
@@ -71,7 +72,19 @@ public class Main {
                 System.out.print("돈을 투입해주세요 : ");
                 int money = Integer.parseInt(sc.nextLine());
                 System.out.println("현재 투입된 금액은 " + money + "원입니다.");
+                remainder = remainder + money;
+
             } else if(menu == 2) {
+                System.out.print("음료수를 선택해주세요 : ");
+                int targetNo = Integer.parseInt(sc.nextLine());
+
+                if(remainder < drinkPrices[targetNo]) {
+                    System.out.println("잔액이 부족합니다.");
+                    continue;
+                }
+
+                System.out.println(drinkNames[targetNo] + "을/를 뽑으셨습니다.");
+                remainder = remainder - drinkPrices[targetNo];
 
             } else if(menu == 3) {
                 System.out.println("=== 음료수 목록 ===");
