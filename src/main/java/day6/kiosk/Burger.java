79a7aa592edd6930436e9347779c448841984a35
chacha86 <taejincha0619@gmail.com>
2024-08-30 16:17:40 +0900

    burger select

---
+++ src/main/java/day6/kiosk/Burger.java
@@ -0,0 +1,11 @@
+package day6.kiosk;
+
+public class Burger {
+    String name;
+    int price;
+
+    public Burger(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
