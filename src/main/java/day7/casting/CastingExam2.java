a2f962376a0c0e83bde1fbc991d4fd782ea116c6
chacha86 <taejincha0619@gmail.com>
2024-09-02 14:37:49 +0900

    casting exam2

---
+++ src/main/java/day7/casting/CastingExam2.java
@@ -0,0 +1,9 @@
+package day7.casting;
+
+public class CastingExam2 {
+    public static void main(String[] args) {
+        Weapon weapon = new Weapon();
+        weapon = new Bow();
+        weapon = new Spear();
+    }
+}
