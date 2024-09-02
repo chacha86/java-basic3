fd73ba80eaad3e809c3ccf71fbb319b5569f4e69
chacha86 <taejincha0619@gmail.com>
2024-09-02 10:54:02 +0900

    constructor chain & override

---
+++ src/main/java/day7/constructorChain/ConstructorChain.java
@@ -0,0 +1,29 @@
+package day7.constructorChain;
+
+public class ConstructorChain {
+    public static void main(String[] args) {
+
+        WhiteDuck wd = new WhiteDuck();
+        wd.fly();
+    }
+}
+
+class Duck {
+    public Duck() {
+        System.out.println("Duck이 생성됩니다.");
+    }
+    public void fly() {
+        System.out.println("오리가 납니다.");
+    }
+}
+
+class WhiteDuck extends Duck {
+    public WhiteDuck() {
+        System.out.println("WhiteDuck이 생성됩니다.");
+    }
+
+    public void fly() {
+        System.out.println("흰오리가 납니다.");
+    }
+
+}
