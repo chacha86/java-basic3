683a858245990180172c7bad80dd45cfa80c6101
chacha86 <taejincha0619@gmail.com>
2024-09-03 13:54:16 +0900

    req 2. add complete

--- src/main/java/txtBoard/Board.java
+++ src/main/java/txtBoard/Board.java
@@ -1,10 +1,12 @@
 package txtBoard;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Board {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Post> posts = new ArrayList<>();
 
         while(true) {
             System.out.print("명령어 : ");
@@ -13,6 +15,18 @@ public class Board {
             if(command.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            } else if(command.equals("add")) {
+                System.out.print("게시물 제목을 입력해주세요 : ");
+                String title = sc.nextLine();
+                System.out.print("게시물 내용을 입력해주세요 : ");
+                String body = sc.nextLine();
+
+                Post post = new Post(title, body);
+//                post.setTitle(title);
+//                post.setBody(body);
+
+                posts.add(post);
+                System.out.println("게시물이 등록되었습니다.");
             }
         }
     }
