8ed34b72f365a55ad1fd137a4f51550551660415
chacha86 <taejincha0619@gmail.com>
2024-09-04 09:45:52 +0900

    object parameter & return exam

--- src/main/java/txtBoard/Board.java
+++ src/main/java/txtBoard/Board.java
@@ -9,7 +9,7 @@ public class Board {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Post> posts = new ArrayList<>();
-        int lastestId = 1; // 가장 최신의 id값. id값의 고유성을 유지하기 위해 1씩 증가시킬 계획임.
+        int lastestId = 1;
 
         while (true) {
             System.out.print("명령어 : ");
@@ -24,24 +24,12 @@ public class Board {
                 System.out.print("게시물 내용을 입력해주세요 : ");
                 String body = sc.nextLine();
 
-                // 1부터 1씩 증가 -> 고유값 유지하는데 편리
-
                 Post post = new Post(lastestId, title, body);
-//                post.setTitle(title);
-//                post.setBody(body);
-
                 posts.add(post);
                 System.out.println("게시물이 등록되었습니다.");
-                lastestId++; // 다음 게시물의 id를 부여하기 위해 1 증가
+                lastestId++;
 
             } else if (command.equals("list")) {
-//                System.out.println("==================");
-//                for(int i = 0; i < posts.size(); i++) {
-//                    System.out.printf("번호 : %d\n", posts.get(i).getId());
-//                    System.out.printf("제목 : %s\n", posts.get(i).getTitle());
-//                    System.out.printf("내용 : %s\n", posts.get(i).getBody());
-//                    System.out.println("==================");
-//                }
 
                 System.out.println("==================");
                 for (Post post : posts) {
@@ -53,14 +41,6 @@ public class Board {
 
                 System.out.print("수정할 게시물 번호 : ");
                 int targetId = Integer.parseInt(sc.nextLine());
-
-                // 논리연산자
-                // 조건1 || 조건2 -> 조건1 조건2 둘 중 하나만 만족해도 실행
-                // 조건1 && 조건2 -> 조건1 조건2가 동시에 만족(모두 만족)해야만 실행
-//                if(targetIdx < 1 || targetIdx > posts.size()) {
-//                    System.out.println("없는 게시물 번호입니다.");
-//                    continue;
-//                }
 
                 for (Post post : posts) {
                     if (post.getId() == targetId) {
@@ -76,20 +56,13 @@ public class Board {
                     }
                 }
 
-
-//                Post newPost = new Post(newTitle, newBody);
-//                posts.set(targetIdx - 1, newPost); // 원하는 위치의 post를 newPost로 교체
-
-//                Post post = posts.get(targetIdx - 1);
-//                post.setTitle(newTitle);
-//                post.setBody(newBody);
             } else if(command.equals("delete")) {
                 System.out.print("삭제할 게시물 번호 : ");
                 int targetId = Integer.parseInt(sc.nextLine());
 
                 for (Post post : posts) {
                     if (post.getId() == targetId) {
-                        posts.remove(post); // index가 아닌 값을 넣어서 해당 값을 삭제할 수도 있다.
+                        posts.remove(post);
                         System.out.println("삭제가 완료되었습니다.");
                         break;
                     }
---
+++ src/main/java/txtBoard/test/Test.java
@@ -0,0 +1,51 @@
+package txtBoard.test;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+
+        setPerson(p1);
+
+        p1.intro();
+
+        Person p2 = getPerson();
+
+        p2.intro();
+
+        ArrayList<String> arr = new ArrayList<>();
+
+
+
+    }
+
+    // main에서 arr을 넘겨받아 "apple", "banana", "orange" 저장
+    public static void setArr() {
+
+    }
+
+    public static Person getPerson() {
+        Person p = new Person();
+        p.age = 30;
+        p.name = "이순신";
+
+        return p;
+    }
+
+    public static void setPerson(Person a) {
+        a.age = 20;
+        a.name = "홍길동";
+    }
+}
+
+class Person {
+    int age;
+    String name;
+
+    public void intro() {
+        System.out.printf("%d살 %s입니다.\n", age, name);
+    }
+}
+
+
