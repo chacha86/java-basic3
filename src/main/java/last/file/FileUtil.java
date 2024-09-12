f535a328a7d11a269e32140b4c4f3c7faccad685
chacha86 <taejincha0619@gmail.com>
2024-09-12 11:49:35 +0900

    file util create

---
+++ src/main/java/last/file/FileUtil.java
@@ -0,0 +1,38 @@
+package last.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtil {
+
+    public void save(String content, String filename) {
+
+        Path filePath = Paths.get(filename);
+
+        try {
+            Files.write(filePath, content.getBytes());
+            System.out.println("파일 작성 완료: " + filePath.toAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String load(String filename) {
+
+        Path filePath = Paths.get(filename);
+
+        try {
+            // 파일에서 문자열 읽기
+            String content = Files.readString(filePath);
+            return content;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
